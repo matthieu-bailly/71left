c21aecd1d6ef5c429af61c58b44299f00cbca56c
matthieu-bailly <baillymatthieu78@yahoo.fr>
2018-03-29 00:34:23 +0200

    Commit last night
    Go php debug go - change jquery page stat

--- platforms/android/app/src/main/assets/www/js/bddstat.js
+++ platforms/android/app/src/main/assets/www/js/bddstat.js
@@ -37,15 +37,34 @@ $(".done").on('click',function(){
     });
 });
 
-$('#pourcentage').on('click',function(){
+$(".start").on('click',function(){
+
+
+
+        
+        $.ajax({
+
+      type: "POST",
+      url : 'http://venus.iut-velizy.uvsq.fr/~mbailly/php/cordova_traitement.php',
+      data : {order : $(this).attr('id')},
+      success: function(retour)
+      {
+        alert(retour);
+
+      }
+
+    });
+});
+
+$('.pourcentages').ready(function(){
 $.ajax({
 
       type: "POST",
       url : 'http://venus.iut-velizy.uvsq.fr/~mbailly/php/cordova_traitement.php',
-      data : {order : "stat1"},
+      data : {order : $(this).attr('id')},
       success: function(retour)
       {
-        $('#pourcentageres').html(retour);
+        $(this).attr('id').html(retour);
 
       }
 
--- www/js/bddstat.js
+++ www/js/bddstat.js
@@ -56,15 +56,47 @@ $(".start").on('click',function(){
     });
 });
 
-$('.pourcentages').ready(function(){
+$('#pourcentage1').ready(function(){
 $.ajax({
 
       type: "POST",
       url : 'http://venus.iut-velizy.uvsq.fr/~mbailly/php/cordova_traitement.php',
-      data : {order : $(this).attr('id')},
+      data : {order : "pourcentage1"},
       success: function(retour)
       {
-        $(this).attr('id').html(retour);
+        $(this).html(retour);
+
+      }
+
+    });
+
+});
+
+$('#pourcentage2').ready(function(){
+$.ajax({
+
+      type: "POST",
+      url : 'http://venus.iut-velizy.uvsq.fr/~mbailly/php/cordova_traitement.php',
+      data : {order : "pourcentage2"},
+      success: function(retour)
+      {
+        $(this).html(retour);
+
+      }
+
+    });
+
+});
+
+$('#pourcentage3').ready(function(){
+$.ajax({
+
+      type: "POST",
+      url : 'http://venus.iut-velizy.uvsq.fr/~mbailly/php/cordova_traitement.php',
+      data : {order : "pourcentage3"},
+      success: function(retour)
+      {
+        $(this).html(retour);
 
       }
 
